629b182846e18f0e11d48e5b01b4ff00d7adf0fd
Frederic Mineur <frederic.mineur@vliz.be>
2019-10-31 09:52:01 +0100

    191031

--- src/Controller/CampaignController.php
+++ src/Controller/CampaignController.php
@@ -5,16 +5,15 @@ namespace App\Controller;
 use App\Entity\Campaign;
 use App\Entity\Cruise;
 use App\Entity\Investigators;
-use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DisplayController extends AbstractController
+class CampaignController extends AbstractController
 {
     /**
      * @Route("/cruises", name="cruises_index")
      */
-    public function cruiseIndex()
+    public function allCruisesIndex()
     {
         $repoCruise = $this->getDoctrine()->getRepository(Cruise::class);
         $repoInvestigator = $this->getDoctrine()->getRepository(Investigators::class);
@@ -35,19 +34,25 @@ class DisplayController extends AbstractController
         $campaign = $repoCampaigns->findOneBy(['campaignid'=> $campaignId]);
         $cruises = $campaign->getCruise();
 
-//        $iterator = $cruises->getIterator();
-//        $iterator->uasort(function($a, $b) {
-//            return ($a->getStartdate() < $b->getStartdate()) ? -1 : 1;
-//        });
-//        $collectionCruises = new ArrayCollection(iterator_to_array($iterator));
-//        dump($collectionCruises);
-
-//        dump($cruises);
-
         return $this->render('display/display_campaign.html.twig',[
                 'campaign' => $campaign,
                 'cruises' => $cruises
             ]);
+
+    }
+
+    /**
+     * @Route ("/cruise/{cruiseId}", name="cruise_details")
+     */
+    public function cruiseDetails($cruiseId){
+        $repoCruises = $this->getDoctrine()->getRepository(Cruise::class);
+        $cruise = $repoCruises->findOneBy(['cruiseid'=>$cruiseId]);
+        $campaigns = $cruise->getCampaign();
+
+        return $this->render('display/display_cruise.html.twig', [
+            'campaigns' => $campaigns,
+            'cruise' => $cruise
+        ]);
 
     }
 
