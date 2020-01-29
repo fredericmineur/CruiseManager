fa9d8fd8df5b55c7a90a356df6b12c42356e65ca
Filip Waumans <filipw@vliz.be>
2020-01-29 22:02:20 +0100

    Add proxy route and service for imiswebservice to avoid CORS problems when sending AJAX call to the imis webservice

---
+++ src/Controller/ImisController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: filipw
+ * Date: 29-01-20
+ * Time: 9:38 PM
+ */
+
+namespace App\Controller;
+
+class ImisController
+{
+
+}
---
+++ src/Service/ImisService.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: filipw
+ * Date: 29-01-20
+ * Time: 9:32 PM
+ */
+
+namespace App\Service;
+
+class ImisService
+{
+
+}
