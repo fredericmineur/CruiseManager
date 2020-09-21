c23d75abf59e6504760fe232ff45e9c2c1b018da
Frederic Mineur <frederic.mineur@vliz.be>
2020-09-21 04:11:35 +0200

    form period

--- src/Controller/PeriodController.php
+++ src/Controller/PeriodController.php
@@ -5,8 +5,11 @@ namespace App\Controller;
 
 
 use App\Entity\Periods;
+use App\Form\PeriodType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -29,5 +32,31 @@ class PeriodController extends AbstractController
      */
     public function displayPeriods () {
         return $this->render('display/display_periodsList.html.twig');
+    }
+
+    /**
+     * @Route("/periods/new", name="create_period")
+     */
+    public function createPeriod(EntityManagerInterface $manager, Request $request) {
+        $period = new Periods();
+        $form = $this->createForm(PeriodType::class, $period);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            switch ($period->getShort()){
+                case 'Multi-day trip': $period->setColorcode('red'); break;
+                case 'WEEKEND - HOLIDAY': $period->setColorcode('grey'); break;
+                case 'Maintenance MOB/DEMOB': $period->setColorcode('blue'); break;
+            }
+            $manager->persist($period);
+            $manager->flush();
+            return $this->redirectToRoute('display_periods', [
+
+            ]);
+        }
+
+        return $this->render('forms/form_period.html.twig', [
+            'formPeriod' => $form->createView(),
+            'period' => $period
+        ]);
     }
 }
--- src/Form/PeriodType.php
+++ src/Form/PeriodType.php
@@ -4,19 +4,71 @@ namespace App\Form;
 
 use App\Entity\Periods;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class PeriodType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('startdate')
-            ->add('enddate')
-            ->add('short')
-            ->add('description')
-            ->add('colorcode')
+            ->add(
+                'startdate',
+                DateType::class,
+                [
+                    'widget'=>'single_text',
+                    'label'=>'Start Date',
+                    'required' => true,
+                    'html5'=> false,
+                    'format'=> 'yyyy-MM-dd',
+                    'attr'=>[
+                        'placeholder' => 'YYYY-mm-dd',
+                        'class' => 'js-datepicker'
+                    ],
+                    'constraints' => [
+                        new NotBlank(),
+                        new Date()
+                    ]
+                ]
+            )
+            ->add(
+                'enddate',
+                DateType::class,
+                [
+                    'widget'=>'single_text',
+                    'label'=>'End Date',
+                    'required' => true,
+                    'html5'=> false,
+                    'format'=> 'yyyy-MM-dd',
+                    'attr'=>[
+                        'placeholder' => 'YYYY-mm-dd',
+                        'class' => 'js-datepicker'
+                    ],
+                    'constraints' => [
+                        new NotBlank(),
+                        new Date(),
+                        new GreaterThanOrEqual([
+                            'propertyPath' =>'parent.all[startdate].data'
+                        ])
+                    ]
+                ]
+                )
+            ->add('short',
+                ChoiceType::class,[
+                    'label' => 'Category',
+                    'choices' => [
+                        'Multi-day trip' => 'Multi-day trip',
+                        'WEEKEND - HOLIDAY' => 'WEEKEND - HOLIDAY',
+                        'Maintenance MOB/DEMOB' =>'Maintenance MOB/DEMOB'
+                    ]
+                ])
+//            ->add('description')
+//            ->add('colorcode')
         ;
     }
 
